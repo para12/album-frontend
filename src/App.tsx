bfbed82eea0b96b502419edba03c319a8f9ab946
hyunwoo lee <hyunwooda@gmail.com>
2021-08-18 00:22:12 +0900

    connect to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Router from './Router'
 
-function App() {
+// type activeType = {
+//   active: boolean;
+// };
+
+const FullContainer = styled.div`
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  width : 80% auto;
+`;
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <FullContainer>
+        <Router />
+      </FullContainer>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import GlobalStyle from "./assets/styles/global-styles";
+import { theme } from "./assets/styles/theme";
+import { ThemeProvider } from 'styled-components';
+
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/app/graphql",
+});
+
+
+const token = "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InBhcmExMiIsImV4cCI6MTYyOTIxMTQ1Miwib3JpZ0lhdCI6MTYyOTIxMTE1Mn0.KaU1Fu_NnCpM3Co7Ms0hes87I813RtAWBH9o7IyhFXk"
+const authLink = setContext((_, { headers }) => {
+  // const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
